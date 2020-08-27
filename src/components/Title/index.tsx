f0f441538d899859886d8ae2932bb2f5d46345b1
mpvinnie <vinniemalafaia@hotmail.com>
2020-08-27 03:47:19 -0400

    Heading

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const Title: React.FC = () => {
+  return (
+    <Container>
+      <Text>Title</Text>
+    </Container>
+  );
+};
+
+export default Title;
